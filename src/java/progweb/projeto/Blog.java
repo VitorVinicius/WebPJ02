531d1c0d99659bd755155ce9882d3fe22c19d13b
Aluno <Aluno@A040-117351>
2018-05-18 17:26:52 -0300

    Listagem dos posts

---
+++ src/java/progweb/projeto/Blog.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progweb.projeto;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Blog {
+    public static ArrayList<Postagem> getPostagens(){
+            ArrayList<Postagem> postagens = new ArrayList();
+        try {
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+             Connection con = DriverManager.getConnection("jdbc:mysql://localhost/blog", "root", "utfpr");
+                    String consulta = "select * from postagem";
+                    PreparedStatement stmt = con.prepareStatement (consulta);
+                    
+                    ResultSet rs = stmt.executeQuery();
+                    
+                    while(rs.next()){
+                        
+                        
+                        Postagem p = new Postagem(rs.getString("titulo"),rs.getString("imagem"),rs.getString("texto"));
+                        postagens.add(p);
+                    }
+                    
+                     
+        } catch (SQLException ex) {
+            Logger.getLogger(Blog.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return postagens;
+    }
+}
+    
+
---
+++ src/java/progweb/projeto/Postagem.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progweb.projeto;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Postagem {
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Postagem(String titulo, String imagem, String texto) {
+        this.titulo = titulo;
+        this.imagem = imagem;
+        this.texto = texto;
+    }
+    String titulo;
+    String imagem;
+    String texto;
+}
